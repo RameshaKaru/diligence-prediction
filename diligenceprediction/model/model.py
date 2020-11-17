4afc1e12e0843bd209f5faa45145f4c6e4d3739e
Ramesha Karunasena <ramesha.karu@gmail.com>
2020-11-17 14:59:26 +0800

    Update models

--- diligenceprediction/model/model.py
+++ diligenceprediction/model/model.py
@@ -35,3 +35,36 @@ def cmlstmModel(num_rules=18, lstm_units=50, num_months=6, dense_param=[2]):
     return model
 
 
+def normModel(num_rules=18, lstm_units=50, num_months=6, dense_param=[2, 1]):
+    """
+      Model with simple norm score labels
+
+      Parameters
+      ----------
+      num_rules : int
+          number of rules/ number of non-diligence probabilities per ANM per timeframe
+      lstm_units : int
+          number of lstm units
+      num_months :  int
+          number of months in non-diligence vector history taken as input
+      dense_param : int
+          dense layer parameter
+
+      Returns
+      -------
+      model : compiled model
+    """
+
+    first_input = Input(shape=(num_months, num_rules))
+    second_lstm = LSTM(lstm_units)(first_input)
+    third_dense = Dense(dense_param[0], activation="relu")(second_lstm)
+    fourth_dense = Dense(dense_param[1])(third_dense)
+
+    model = Model(inputs=first_input, outputs=fourth_dense)
+
+    model.compile(optimizer='adam', loss='mse',
+                  metrics=['mse', 'mae'])
+
+    return model
+
+
